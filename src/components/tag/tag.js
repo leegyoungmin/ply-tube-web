1b3ca5ad5f8f9f756ea61f5fd17d3db2b144445d
Ubuntu <ubuntu@ip-172-31-63-122.ap-northeast-2.compute.internal>
2024-09-22 10:59:06 +0000

    feat : 머신러닝 결과 구현

--- src/components/tag/tag.js
+++ src/components/tag/tag.js
@@ -5,15 +5,6 @@ import { useNavigate } from 'react-router-dom';
 
 //43.203.164.87:8000
 
-const musicListData = {
-    "태연": ["Something New - 태연", "I - 태연"],
-    "Get Up": ["Get Up - NewJeans", "Hype Boy - NewJeans"],
-    "NewJeans": ["Attention - NewJeans", "Ditto - NewJeans"],
-    "I'm in love": ["I'm in love - 성시경", "All of Me - John Legend"],
-    "Candy for you": ["Candy - 백현", "Candy Shop - 50 Cent"],
-    "로시": ["Stars - 로시", "Like a Star - Corinne Bailey Rae"]
-};
-
 function getRandomColor() {
     const randomColors = ['#FF0000', '#E7DA29', '#2614AB', '#77FFD6']
     const randomIndex = Math.floor(Math.random() * randomColors.length);
@@ -34,23 +25,30 @@ function hexToRgb(hex) {
 function TagCloud() {
     const navigate = useNavigate(); // useHistory 훅 사용
 
-    const navigateToResult = () => {
+    const navigateToResult = async () => {
         // 선택된 태그와 관련된 음악 목록 생성
-        const selectedKeywords = Object.keys(selectedTags);
-        const keywordList = selectedKeywords.flatMap(index => {
-            return tags[index].name;
-        })
-        const musicList = selectedKeywords.flatMap(index => {
-            const tagName = tags[index].name; // 인덱스를 사용하여 태그 이름을 가져옴
-            return musicListData[tagName] || []; // 태그 이름으로 musicListData에서 음악 목록 가져옴
-        });
+        const keywords = Object.keys(selectedTags);
 
-        keywordList.map((keyword, index) => (
-            console.log("Keyword", keyword, 'index', index)
-        ))
+        try {
+            const response = await fetch('http://3.38.28.112:8000/submit-selection/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "select_value": keywords
+                }),
+            });
+
+            const result = await response.json();
+            const keywordList = result['keywords']
+            const musicList = result['predictions']
+            navigate('/result', { state: { keywordList, musicList } });
+        } catch (error) {
+            console.error('Error sending POST request')
+        }
 
         // Result 페이지로 이동하면서 데이터 전달
-        navigate('/result', { state: { keywordList, musicList } });
     };
 
     const [tags, setTag] = useState([]);
@@ -59,7 +57,7 @@ function TagCloud() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('http://43.203.164.87:8000/random-items/');
+                const response = await fetch('http://3.38.28.112:8000/random-items/');
                 const result = await response.json();
                 console.log(result);
                 setTag(result['keywords'])
@@ -91,6 +89,7 @@ function TagCloud() {
                 }
             }));
         }
+        console.log(selectedTags)
     };
 
     return (
@@ -108,11 +107,7 @@ function TagCloud() {
                     {tag}
                 </button>
             ))}
-            <button className='create-playlist' onClick={navigateToResult}>
-                <img src='/images/icon_puzzle.svg' alt="Icon" className="header-icon" />
-                추가하기
-            </button>
-            <button className='create-playlist' onClick={navigateToResult}>
+            <button className='create-playlist' onClick={navigateToResult} disabled={Object.keys(selectedTags).length === 0}>
                 <img src='/images/icon_puzzle.svg' alt="Icon" className="header-icon" />
                 플레이리스트 만들기
             </button>
